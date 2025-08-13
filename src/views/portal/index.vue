1e5fd2020fc5d60b3ce7e2e58785865f32138c48
WyattMac <wyatt6@163.com>
2025-08-13 22:43:50 +0800

    Add footer

--- src/views/portal/index.vue
+++ src/views/portal/index.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="page-wrap">
-    <el-card class="main-card">
-      <div class="half-card cover-img">
-        <div class="cover-text">
-          <div class="cover-title">闲来 Admax</div>
-          <div class="cover-subtitle">一款开源、轻量的后台管理系统</div>
+    <div class="card-wrap">
+      <el-card class="main-card">
+        <div class="half-card cover-img">
+          <div class="cover-text">
+            <div class="cover-title">闲来 Admax</div>
+            <div class="cover-subtitle">一款开源、轻量的后台管理系统</div>
+          </div>
         </div>
-      </div>
-      <div class="half-card input-box">
-        <RouterView />
-      </div>
-    </el-card>
+        <div class="half-card input-box">
+          <RouterView />
+        </div>
+      </el-card>
+    </div>
+    <div class="footer">
+      <span>
+        Copyright &copy; 2025-2025 XianLai.fun 闲来. All rights reserved.&nbsp;
+        <a target="_blank" href="https://beian.miit.gov.cn/">粤ICP备XXXXXXXX号</a>&nbsp;
+        <a target="_blank" href="https://beian.mps.gov.cn/">粤公网安备XXXXXXXX号</a>
+      </span>
+    </div>
   </div>
 </template>
 
@@ -18,63 +27,79 @@
 .page-wrap {
   width: 100%;
   height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   background-color: var(--el-bg-color-page);
+  $--footer-height: 4rem;
 
-  .main-card {
-    $--main-card-width: 100rem;
-    $--main-card-height: 60rem;
-    width: $--main-card-width;
-    height: $--main-card-height;
-    border-radius: 0;
+  .card-wrap {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-    :deep(.el-card__body) {
-      width: 100%;
-      height: 100%;
-      padding: 0;
-      display: flex;
-      align-items: center;
-      justify-content: space-around;
-    }
+    .main-card {
+      $--main-card-width: 100rem;
+      $--main-card-height: 60rem;
+      width: $--main-card-width;
+      height: $--main-card-height;
+      border-radius: 0;
 
-    .half-card {
-      width: 50%;
-      height: 100%;
-    }
+      :deep(.el-card__body) {
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+      }
 
-    .cover-img {
-      background-image: url('@/assets/images/portal-cover.jpg');
-      background-size: 100% 100%;
-      display: flex;
-      justify-content: center;
+      .half-card {
+        width: 50%;
+        height: 100%;
+      }
 
-      .cover-text {
-        width: 80%;
-        height: 10rem;
-        margin-top: 9rem;
-        cursor: default;
+      .cover-img {
+        background-image: url('@/assets/images/portal-cover.jpg');
+        background-size: 100% 100%;
+        display: flex;
+        justify-content: center;
 
-        .cover-title {
-          font-size: 5rem;
-          font-weight: 900;
-          color: #ffffff;
-        }
+        .cover-text {
+          width: 80%;
+          height: 10rem;
+          margin-top: 9rem;
+          cursor: default;
 
-        .cover-subtitle {
-          font-size: 2rem;
-          color: #ffffff;
+          .cover-title {
+            font-size: 5rem;
+            font-weight: 900;
+            color: #ffffff;
+          }
+
+          .cover-subtitle {
+            font-size: 2rem;
+            color: #ffffff;
+          }
         }
       }
-    }
 
-    .input-box {
-      $--input-box-padding: 7rem;
-      padding: $--input-box-padding;
-      width: calc($--main-card-width / 2 - 2 * $--input-box-padding);
-      height: calc($--main-card-height - 2 * $--input-box-padding);
+      .input-box {
+        $--input-box-padding: 7rem;
+        padding: $--input-box-padding;
+        width: calc($--main-card-width / 2 - 2 * $--input-box-padding);
+        height: calc($--main-card-height - 2 * $--input-box-padding);
+      }
     }
+  }
+
+  .footer {
+    width: 100%;
+    height: $--footer-height;
+    margin-top: -$--footer-height;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.2rem;
   }
 }
 </style>
