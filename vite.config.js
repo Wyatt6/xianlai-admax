fedbccb75a25641b439e6a859f6a4b499e0b70e3
WyattMac <wyatt6@163.com>
2025-08-18 15:13:41 +0800

    创建工程

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// vite配置: https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
