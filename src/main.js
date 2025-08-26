a74b5ccfb2005749f37215d649886e03f9d46a32
WyattMac <wyatt6@163.com>
2025-08-26 10:28:06 +0800

    引入element-plus；初始化获取后端系统参数

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
+// 有坑：样式的引入需要放在前面
+import ElementPlus from 'element-plus'
+import zhCN from 'element-plus/es/locale/lang/zh-cn'
+import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { useSystemStore } from './stores/system'
 
 const app = createApp(App)
 
+app.use(ElementPlus, { locale: zhCN })
 app.use(createPinia())
 app.use(router)
+
+const systemStore = useSystemStore()
+await systemStore.getSystemOptions()
 
 app.mount('#app')
---
+++ src/stores/system.js
@@ -0,0 +1,43 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+
+export const useSystemStore = defineStore('system', () => {
+  const gettingSystemOptions = ref(false)
+  const systemOptions = ref(null)
+  const systemOptionsChecksum = ref(null)
+
+  async function getSystemOptions() {
+    if (!gettingSystemOptions.value) {
+      gettingSystemOptions.value = true
+      await axios
+        .get('/api/admax/system/system-option/getSystemOptions', {
+          headers: { 'Content-Type': 'application/json; charset=utf-8' },
+          timeout: 60000
+        })
+        .then(reponse => {
+          const result = reponse.data
+          if (result.success) {
+            systemOptions.value = result.data.systemOptions
+            systemOptionsChecksum.value = result.data.systemOptionsChecksum
+          } else {
+            ElMessage.error('初始化失败，请稍后再试')
+          }
+        })
+        .catch(error => {
+          ElMessage.error('初始化失败')
+          console.error(error)
+        })
+        .finally(() => {
+          gettingSystemOptions.value = false
+        })
+    }
+  }
+
+  return {
+    systemOptions,
+    systemOptionsChecksum,
+    getSystemOptions
+  }
+})
