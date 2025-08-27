7f05a0e3ad84f8bf3c1bc5cbfdd831f2e514a13a
WyattMac <wyatt6@163.com>
2025-08-27 23:13:59 +0800

    修复bug

--- src/apis/eval.js
+++ src/apis/eval.js
@@ -3,26 +3,31 @@ import axios from 'axios'
 import { useOptionStore } from '@/options'
 import { notEmpty } from '@/utils/common'
 
-const Options = useOptionStore()
+function createAxiosInstance() {
+  const Options = useOptionStore()
 
-/**
- * 创建axios对象实例
- *
- * baseURL: 自动添加的请求url前缀
- * headers: 请求头信息
- * timeout: 请求超时时间，单位毫秒
- */
-const instance = axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json; charset=utf-8' },
-  timeout: Options.data.request.timeout
-  // ...其他配置使用axios的默认值
-})
+  /**
+   * 创建axios对象实例
+   *
+   * baseURL: 自动添加的请求url前缀
+   * headers: 请求头信息
+   * timeout: 请求超时时间，单位毫秒
+   */
+  const instance = axios.create({
+    baseURL: '/api',
+    headers: { 'Content-Type': 'application/json; charset=utf-8' },
+    timeout: Options.data.request.timeout
+    // ...其他配置使用axios的默认值
+  })
+
+  return instance
+}
 
 // TODO 请求/响应拦截器
 
 export function evalApis(request, result) {
   request.value = {}
+  const axiosInstance = createAxiosInstance()
 
   for (var i = 0; i < result.length; i++) {
     const { callPath, requestMethod, url, description } = result[i]
@@ -36,7 +41,7 @@ export function evalApis(request, result) {
           if (description && description.length > 0 && description != 'null') config.desc = description
           if (notEmpty(requestParams)) config.params = requestParams
           if (notEmpty(requestData)) config.data = requestData
-          return instance(config)
+          return axiosInstance(config)
         }
       }
       now = now[paths[j]]
