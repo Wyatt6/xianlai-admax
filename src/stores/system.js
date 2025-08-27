1605069ef14f7445ec6e538c3e0fd9ef27805010
WyattMac <wyatt6@163.com>
2025-08-27 11:24:06 +0800

    添加加载系统接口初始化功能

--- src/stores/system.js
+++ src/stores/system.js
@@ -4,15 +4,50 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 
 export const useSystemStore = defineStore('system', () => {
+  const gettingApis = ref(false)
+  const apis = ref(null)
+  const apisChecksum = ref(null)
+
+  async function getApis() {
+    if (!gettingApis.value) {
+      gettingApis.value = true
+      await axios
+        .get('/api/admax/system/api/getApis', {
+          headers: { 'Content-Type': 'application/json; charset=utf-8' },
+          timeout: 60000
+        })
+        .then(response => {
+          const result = response.data
+          if (result.success) {
+            apis.value = result.data.apis
+            apisChecksum.value = result.data.apisChecksum
+            // TODO 封装系统接口对象
+          } else {
+            ElMessage.error('无法获取系统接口，请稍后再试')
+            // TODO 跳转到错误页面
+          }
+        })
+        .catch(error => {
+          ElMessage.error('无法获取系统接口，请稍后再试')
+          // TODO 跳转到错误页面
+          console.error(error)
+        })
+        .finally(() => {
+          gettingApis.value = false
+        })
+    }
+  }
+
   const gettingSystemOptions = ref(false)
   const systemOptions = ref(null)
   const systemOptionsChecksum = ref(null)
 
   async function initialize() {
+    await getApis()
+
     if (!gettingSystemOptions.value) {
       gettingSystemOptions.value = true
       await axios
-        .get('/api/admax/system/system-option/getSystemOptions', {
           headers: { 'Content-Type': 'application/json; charset=utf-8' },
           timeout: 60000
         })
