dcdad9ca8e93854c9de56eeb70d27e69f32badb5
WyattMac <wyatt6@163.com>
2025-08-29 17:05:36 +0800

    系统内置路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,31 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/portal',
+      redirect: '/portal/login',
+      name: 'portal',
+      component: () => import('@/views/portal/index.vue'),
+      children: [
+        {
+          path: '/portal/login',
+          name: 'login',
+          component: () => import('@/views/portal/login/index.vue')
+        },
+        {
+          path: '/portal/register',
+          name: 'register',
+          component: () => import('@/views/portal/register/index.vue')
+        },
+        {
+          path: '/portal/reset-password',
+          name: 'reset_password',
+          component: () => import('@/views/portal/reset_password/index.vue')
+        }
+      ]
+    }
+  ]
 })
 
 export default router
