ba423d6c075cc17105024981ca2570cdffff22cb
WyattMac <wyatt6@163.com>
2025-09-02 16:29:41 +0800

    优化系统参数赋值逻辑

--- src/options/eval.js
+++ src/options/eval.js
@@ -1,4 +1,4 @@
-import { hasText } from '@/utils/common'
+import { hasText, isEmpty } from '@/utils/common'
 
 export const defaultOptions = {
   console: {
@@ -8,24 +8,32 @@ export const defaultOptions = {
     timeout: 10000
   },
   captcha: {
-    length: 5,
-    expireSeconds: 60
+    length: 5
   }
 }
 
 export function evalOptions(data, result) {
   data.value = defaultOptions
-
-  if (hasText(result['console.openLog'])) {
-    data.value.console.openLog = result['console.openLog'] == 'true'
-  }
-  if (hasText(result['request.timeout'])) {
-    data.value.request.timeout = Number(result['request.timeout'])
-  }
-  if (hasText(result['captcha.length'])) {
-    data.value.captcha.length = Number(result['captcha.length'])
-  }
-  if (hasText(result['captcha.expireSeconds'])) {
-    data.value.captcha.expireSeconds = Number(result['captcha.expireSeconds'])
-  }
+  Object.entries(result).forEach(([key, valueObj]) => {
+    if (hasText(valueObj.type) && hasText(valueObj.value)) {
+      const keys = key.split('.')
+      let now = data.value
+      for (let i = 0; i < keys.length; i++) {
+        if (i == keys.length - 1) {
+          if (valueObj.type === 'Boolean') {
+            now[keys[i]] = valueObj.value == 'true'
+          }
+          if (valueObj.type === 'Number') {
+            now[keys[i]] = Number(valueObj.value)
+          }
+          if (valueObj.type === 'String') {
+            now[keys[i]] = valueObj.value
+          }
+        } else {
+          if (isEmpty(now[keys[i]])) now[keys[i]] = {}
+          now = now[keys[i]]
+        }
+      }
+    }
+  })
 }
