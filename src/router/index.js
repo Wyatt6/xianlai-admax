04e5f7e1c443eca8115caa0d6d18943fc8e7e9ff
WyattMac <wyatt6@163.com>
2025-09-02 11:33:03 +0800

    优化路由表

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const builtInRoutes = [
+  {
+    path: '/portal',
+    redirect: '/portal/login',
+    name: 'portal',
+    component: () => import('@/views/portal/index.vue'),
+    children: [
+      {
+        path: '/portal/login',
+        name: 'login',
+        component: () => import('@/views/portal/login/index.vue')
+      },
+      {
+        path: '/portal/register',
+        name: 'register',
+        component: () => import('@/views/portal/register/index.vue')
+      },
+      {
+        path: '/portal/reset-password',
+        name: 'reset_password',
+        component: () => import('@/views/portal/reset_password/index.vue')
+      }
+    ]
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/portal',
-      redirect: '/portal/login',
-      name: 'portal',
-      component: () => import('@/views/portal/index.vue'),
-      children: [
-        {
-          path: '/portal/login',
-          name: 'login',
-          component: () => import('@/views/portal/login/index.vue')
-        },
-        {
-          path: '/portal/register',
-          name: 'register',
-          component: () => import('@/views/portal/register/index.vue')
-        },
-        {
-          path: '/portal/reset-password',
-          name: 'reset_password',
-          component: () => import('@/views/portal/reset_password/index.vue')
-        }
-      ]
-    }
-  ]
+  routes: builtInRoutes
 })
+
+/**
+ * 路由前置守卫：每次路由切换时先执行的操作
+ * @param {*} to   要到哪里去
+ * @param {*} from 从哪里来
+ * @param {*} next 是否要去？
+ */
+router.beforeEach((to, from, next) => {})
 
 export default router
